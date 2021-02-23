b7796b546446a4d8c29168b33635edd948cd495c
Alex Pc <Alex Pc@Alex-HP>
2021-02-23 18:41:19 +0100

    primera versión

---
+++ DiasSemana/src/DiasSemana/DiasSemana.java
@@ -0,0 +1,34 @@
+package DiasSemana;
+
+import java.util.Scanner;
+
+public class DiasSemana {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
+		int N = 0;
+		String diaR="";
+		System.out.println("Introduce un Número: ");
+		N = reader.nextInt();
+		if (N == 1)
+			diaR="Lunes";
+		else if (N == 2)
+			diaR="Martes";
+		else if (N == 3)
+			diaR="Miércoles";
+		else if (N == 4)
+			diaR="Jueves";
+		else if (N == 5)
+			diaR="Viernes";
+		else if (N == 6)
+			diaR="Sábado";
+		else if (N == 7)
+			diaR="Domingo";
+		else
+			diaR="Número no válido";
+		reader.close();
+		System.out.println (diaR);
+	}
+}
